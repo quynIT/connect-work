4afea3c40a4dd4e37a9466567403122585fdd6cc
quynIT <trungquyen2902@gmail.com>
2024-11-08 06:25:29 +0700

    update-page404

--- frontend/src/pages/NoPage.tsx
+++ frontend/src/pages/NoPage.tsx
@@ -2,10 +2,31 @@ import React from "react";
 
 export default function NoPage() {
   return (
-    <div>
-      <h3>Sorry! 404</h3>
-      <br />
-      <h1>We can't seem to find the resource you're looking for.</h1>
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <div className="flex flex-col items-center">
+        <div className="text-8xl mb-4">
+          <span role="img" aria-label="sad face">
+            😢
+          </span>
+        </div>
+        <h1 className="text-4xl font-bold text-gray-800">Page not found!</h1>
+        <p className="text-gray-600 mt-2">
+          Sorry, we couldn't find the page you are looking for.
+        </p>
+        <div className="mt-4">
+          <a href="/" className="text-blue-500 hover:underline">
+            Homepage
+          </a>
+          <span className="mx-2">·</span>
+          <a href="/roadmaps" className="text-blue-500 hover:underline">
+            Roadmaps
+          </a>
+          <span className="mx-2">·</span>
+          <a href="/best-practices" className="text-blue-500 hover:underline">
+            Best Practices
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
